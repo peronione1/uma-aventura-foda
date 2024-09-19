564302b492f5b8e6459058133118e2cbed62b13a
peronione1 <00001114619711sp@al.educacao.sp.gov.br>
2024-09-19 14:06:35 -0300

    erros

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const avanca= document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button=> {
-    button.addEventListener('click',function(){
-        const atual= document.querySelector('.ativo');
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
         const proximoPasso= 'passo-'+this.getAttribute('data-proximo')
 
         atual.classList.remove('ativo')
-        document.getElementById(proximoPasso).classList.edd('ativo')
+        document.getElementById(proximoPasso).classList.add('ativo')
 
     })
 }
