7a6eaedbdf4c51a7b150ef4787a754e18f77da1d
peronione1 <00001114619711sp@al.educacao.sp.gov.br>
2024-09-19 13:48:07 -0300

    gay

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const avanca= document.querySelectorAll('.btn-proximo');
 
-avanca.forEach(i
+avanca.forEach(button=> {
+    button.addEventListener('click',function(){
+        const atual= document.querySelector('.ativo');
+        const proximoPasso= 'passo-'+this.getAttribute('data-proximo')
+
+        atual.classList.remove('ativo')
+        document.getElementById(proximoPasso).classList.edd('ativo')
+
+    })
+}
